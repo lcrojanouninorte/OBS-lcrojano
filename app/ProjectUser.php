7e68fc3c2756653b3c3859eec1937b1ecfe4ef50
luis <lcrojano@gmail.com>
2017-11-01 11:04:31 +0000

    ok

--- app/ProjectUser.php
+++ app/ProjectUser.php
@@ -6,19 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectUser extends Model
 {
-	protected $table = 'project_user';
     //
+    protected $table = 'project_user';
+    
     protected $fillable = [
         'role',
     ];
 
     public function users()
     {
-        return $this->belongsTo('App\User',  "user_id", "id");
+        return $this->belongsTo('App\User', "user_id", "id");
     }
 
-        public function projects()
+    public function projects()
     {
-        return $this->belongsTo('App\Project',  "project_id", "id");
+        return $this->belongsTo('App\Project', "project_id", "id");
     }
 }
