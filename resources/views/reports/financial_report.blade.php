52a9fd6a583be7de00f671cba5090e1f7a6ad6d2
Luis C <lcrojano@uninorte.edu.co>
2018-01-30 05:24:09 -0500

    Primera version reporte

--- resources/views/reports/financial_report.blade.php
+++ resources/views/reports/financial_report.blade.php
@@ -7,8 +7,8 @@
  </head>
 	<body  class="">
  
-		<table  border="1">
-			<tr></tr>
+		<table >
+		 
 			<tr>
 				<td></td>
 				<td></td>
@@ -27,15 +27,15 @@
 			<tr></tr>
 			<tr></tr>
 			<tr>
-				<td style="text-align: center" valign="middle"   colspan="10" align="center"><h3>Reporte Finaciero</h3></td>
+				<td style="text-align: center" valign="middle"   colspan="10" align="center"><h3>Reporte Financiero</h3></td>
 			</tr>
-		<tr></tr>
+ 
 		</table>
 
 
 @foreach ($project->results as $indexResult => $result)
-<table class="table-report bordered">
-	 
+<table class="bordered table-report">
+	
 		<tr>
 			<th class="filled-blue" valign="middle" align="center">
 				R{{ $indexResult+1 }}
@@ -50,99 +50,146 @@
 			<th  class="filled-blue" valign="middle">Fin: </th>
 			<th >{{ $result->fecha_fin }}</th>
 		</tr>
- 
+  
 		@foreach ($result->products as $indexProduct => $product)
-			<tr>
-				<td rowspan="{{count($product->budgets)+1}}" class="filled-blue" valign="middle" align="center">
-					P{{ $indexProduct + 1 }}
-				</td>
-				<td colspan="5">
-					Producto {{$indexProduct + 1}}: <span>  {{ $product->desc }}</span>
-				</td>
-				<td  class="filled-blue-light" valign="middle">Inicio: </td>
-				<td >{{ $product->fecha_inicio }}</td>
-				<td  class="filled-blue-light" valign="middle">Fin: </td>
-				<td >{{ $product->fecha_fin }}</td>
-			</tr>
- 
-		 
-			@foreach ($product->budgets as $indexBudgets => $budget)
+			@if(count($product->wallets)>0)
+
 				<tr>
-					<td></td>
-					<td rowspan="{{count( $budget->budgetproducts)+1}}" class="filled-blue-light" valign="middle" align="center">
-						RU {{ ++$indexBudgets }}
+					<td     class="filled-blue" valign="middle" align="center">
+					<b>	P{{ $indexProduct + 1 }} {{$product->rowspan}} </b>
 					</td>
-					<td colspan="8">
-						Rubro {{ $indexBudgets }}: <span>  {{ $budget->titulo }}</span>
+					<td colspan="5">
+						Producto {{$indexProduct + 1}}: <span>  {{ $product->desc }}</span>
 					</td>
-				 
+					<td  class="filled-blue" valign="middle"><b>Inicio: </b></td>
+					<td ><b>{{ $product->fecha_inicio }}</b></td>
+					<td  class="filled-blue" valign="middle"><b>Fin: </b></td>
+					<td ><b>{{ $product->fecha_fin }}</b></td>
 				</tr>
-				@foreach ($budget->budgetproducts as $indexBudgetproduct => $budgetproduct)
-					<tr>
-						<td></td>
-						<td></td>
-
-						<td  class="filled-blue-light" valign="middle" align="center">
-							Descripción {{ ++$indexBudgetproduct }}
-						</td>
-						<td colspan="7">
-							 <span>  {{ $budgetproduct->descripcion }}</span>
-						</td>
-					 
-					</tr>
-
-					@if( count($budgetproduct->wallets)>0) 
-				 
+				
+				@foreach ($product->budgets as $indexBudgets => $budget)
+					@if($budget->total_executed>0)
 						<tr>
-							<td></td>
-							<td></td>
-							<td colspan="3">Descripción</td>
-							<td >tipo</td>
-							<td >Documento Soporte</td>
-							<td >Sena</td>
-							<td >C. Especie</td>
-							<td >C. Efectivo</td>
-							 
+							<td ></td>
+							<td   class="filled-blue-light" valign="middle" align="center">
+								<b>RU {{ ++$indexBudgets }} {{$budget->rowspan}}</b>
+							</td>
+							<td colspan="8">
+								Rubro {{ $indexBudgets }}: <span>  {{ $budget->titulo }}</span>
+							</td>
+						 
 						</tr>
-					
-						@foreach ( $budgetproduct->wallets as $wallet )
-							<tr>
-								<td></td>
-								<td></td>
-								<td  colspan="3">{{$wallet->desc}}</td>
-								<td >{{$wallet->type}}</td>
-								<td >{{$wallet->doc}}</td>
-								<td>
-									@if($wallet->type == "sena")
-										{{$wallet->cantidad}} 
-									@endif
-								</td>
-								<td>
-									@if($wallet->type == "cp")
-										 {{$wallet->cantidad}} 
-									@endif
-								</td>
-								<td>
-									@if($wallet->type == "ce")
-										{{$wallet->cantidad}} 
-									@endif
-								</td>
-							</tr>
-						@endforeach	
-			 
+						
+						@foreach ($budget->budgetproducts as $indexBudgetproduct => $budgetproduct)
+							@if( count($budgetproduct->wallets)>0) 
+								<tr>
+									<td></td>
+									<td></td>
 
+									<td  class="filled-blue-light" valign="middle" align="center">
+										<b>Descripción {{ ++$indexBudgetproduct }}</b>
+									</td>
+									<td colspan="7">
+										 <span>  {{ $budgetproduct->descripcion }}</span>
+									</td>
+								 
+								</tr>
+								
+
+						
+						 
+								<tr>
+									<td  ></td>
+									<td  ></td>
+									<td  class="filled-blue-light2" colspan="3"><b>Detalle</b></td>
+									<td  class="filled-blue-light2"><b>tipo</td>
+									<td  class="filled-blue-light2"><b>Documento Soporte</b></td>
+									<td  class="filled-blue-light2"><b>Sena</b></td>
+									<td  class="filled-blue-light2"><b>C. Especie</b></td>
+									<td  class="filled-blue-light2"><b>C. Efectivo</b></td>
+									 
+								</tr>
+								
+							
+								@foreach ( $budgetproduct->wallets as $wallet )
+									<tr>
+										<td></td>
+										<td></td>
+										<td  colspan="3">{{$wallet->desc}}</td>
+										<td >{{$wallet->type}}</td>
+										<td >{{$wallet->doc}}</td>
+										<td  align="right" valign="center"><b>
+											@if($wallet->type == "sena")
+												{{   $wallet->cantidad  	}} 
+											@endif
+											</b>
+										</td>
+										<td align="right" valign="center">
+											<b>
+											@if($wallet->type == "cp")
+												 {{   $wallet->cantidad }} 
+											@endif
+											</b>
+										</td>
+										<td align="right" valign="center">
+											<b>
+											@if($wallet->type == "ce")
+												{{   $wallet->cantidad }} 
+											@endif
+											</b>
+										</td>
+									</tr>
+									
+								@endforeach
+
+
+								<tr>
+									<td></td>
+									<td></td>
+									<td class="filled-blue-light2" colspan="5" align="right"><b><i>Total Ejecutado</i></b></td>
+									<td align="right" valign="center">
+										<b>
+										{{ (isset($budgetproduct->wallets_executed["sena"])) ? $budgetproduct->wallets_executed["sena"]->total_executed :"0" }}
+										</b>
+									</td>
+									<td align="right" valign="center">
+										<b>
+										{{ (isset($budgetproduct->wallets_executed["cp"])) ? $budgetproduct->wallets_executed["cp"]->total_executed :"0" }}
+										</b>
+									</td>
+									<td align="right" valign="center">
+										<b>
+										{{ (isset($budgetproduct->wallets_executed["ce"])) ? $budgetproduct->wallets_executed["ce"]->total_executed :"0" }}
+										</b>
+									</td>
+									
+								</tr>
+								
+								<tr>
+									<td ></td>
+									<td></td>
+									<td class="filled-blue-light2" colspan="5" align="right"><b><i>Disponible</i></b></td>
+									<td align="right" valign="center"><b>{{(isset($budgetproduct->wallets_executed["sena"])) ?  $budgetproduct->financiacion_sena - $budgetproduct->wallets_executed["sena"]->total_executed  :$budgetproduct->financiacion_sena  }}</b></td>
+									<td align="right" valign="center"><b>{{(isset($budgetproduct->wallets_executed["cp"])) ?  $budgetproduct->c_especie - $budgetproduct->wallets_executed["cp"]->total_executed  :$budgetproduct->c_especie  }}</b></td>
+									<td align="right" valign="center"><b>{{(isset($budgetproduct->wallets_executed["ce"])) ?  $budgetproduct->c_efectivo - $budgetproduct->wallets_executed["ce"]->total_executed  :$budgetproduct->c_efectivo  }}</b></td>
+									
+								</tr>
+									
+					 
+
+							@endif
+						@endforeach
+
+					 
 					@endif
 				@endforeach
 
-			 
+			
+			@endif
+		@endforeach
 
-			@endforeach
-
-		 
-		@endforeach	
  
 </table>
-
 
 @endforeach	
 </body>
