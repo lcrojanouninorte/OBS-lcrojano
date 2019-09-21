948b0f003d0792487fba0039dbd348d681a1b5d6
Luis Rojano <lcrojano@uninorte.edu.co>
2019-09-21 00:49:53 -0500

    70%

--- database/migrations/2019_09_19_230731_create_categories_table.php
+++ database/migrations/2019_09_19_230731_create_categories_table.php
@@ -18,6 +18,8 @@ class CreateCategoriesTable extends Migration
             $table->string('public_desc');
             $table->string('admin_desc');
             $table->string('state');
+            $table->string('icon');
+
             $table->timestamps();
         });
     }
